bd32b57ece2898f8f8e5e7558b0e614d57b72b6f
188128934@qq.com <188128934@qq.com>
2018-02-24 21:29:19 +0800

    1

---
+++ cgi-bin/3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+#coding:utf-8
+import sys
+ARGV_LEN=len(sys.argv)
+for i in range(1,ARGV_LEN):
+        print i
+        print sys.argv[i]
+        print "-----"
---
+++ cgi-bin/translate_platefrom_ready.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+#coding:utf-8
+import os
+
+os.system('echo "`date`" >>/tmp/liuyi_test')
